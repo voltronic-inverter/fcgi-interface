b3dfa1e0b28be2d5ce626877ea321eea365f7b44
Johan van der Vyver <johanvdvyver@Johans-MacBook-Pro.local>
2020-03-13 03:59:24 +0200

    Fix broken char size

--- src/voltronic_dev.c
+++ src/voltronic_dev.c
@@ -6,9 +6,8 @@
 #include <stdlib.h>
 
 #define END_OF_INPUT '\r'
-#define SIZE_OF_CRC (sizeof(voltronic_crc_t))
-#define SIZE_OF_END_OF_INPUT (sizeof(END_OF_INPUT))
-#define MINIMUM_SIZE (SIZE_OF_CRC + SIZE_OF_END_OF_INPUT)
+#define END_OF_INPUT_SIZE sizeof(char)
+#define NON_DATA_SIZE (sizeof(voltronic_crc_t) + END_OF_INPUT_SIZE)
 
 struct voltronic_dev_struct_t {
   void* impl_ptr;
@@ -111,7 +110,6 @@ static int voltronic_read_data_loop(
     const unsigned long timeout_milliseconds) {
 
   unsigned int size = 0;
-  unsigned int parse_complete = 0;
 
   const millisecond_timestamp_t start_time = get_millisecond_timestamp();
   millisecond_timestamp_t elapsed = 0;
@@ -123,37 +121,32 @@ static int voltronic_read_data_loop(
       buffer_length,
       timeout_milliseconds - elapsed);
 
-    if (bytes_read > 0) {
+    if (bytes_read >= 0) {
       while(bytes_read) {
         --bytes_read;
         ++size;
 
         if (*buffer == END_OF_INPUT) {
-          parse_complete = 1;
-          bytes_read = 0;
+          SET_ERRNO(0);
+          return size;
         }
 
-        ++buffer;
+        buffer += sizeof(char);
         --buffer_length;
       }
 
-      if (parse_complete) {
-        SET_ERRNO(0);
-        return size;
+      elapsed = get_millisecond_timestamp() - start_time;
+      if (elapsed >= timeout_milliseconds) {
+        SET_ERRNO(ETIMEDOUT);
+        return -1;
       }
-    } else if (bytes_read != 0) {
+
+      if (buffer_length <= 0) {
+        SET_ERRNO(ENOBUFS);
+        return -1;
+      }
+    } else {
       return bytes_read;
-    }
-
-    elapsed = get_millisecond_timestamp() - start_time;
-    if (elapsed >= timeout_milliseconds) {
-      SET_ERRNO(ETIMEDOUT);
-      return -1;
-    }
-
-    if (buffer_length <= 0) {
-      SET_ERRNO(ENOBUFS);
-      return -1;
     }
   }
 }
@@ -170,10 +163,10 @@ static int voltronic_receive_data(
     buffer_length,
     timeout_milliseconds);
 
-  if (result > 0) {
-    if (((size_t) result) >= MINIMUM_SIZE) {
-      const size_t data_size = result - MINIMUM_SIZE;
-      const voltronic_crc_t read_crc = read_voltronic_crc(&buffer[data_size], MINIMUM_SIZE);
+  if (result >= 0) {
+    if (((size_t) result) >= NON_DATA_SIZE) {
+      const size_t data_size = result - NON_DATA_SIZE;
+      const voltronic_crc_t read_crc = read_voltronic_crc(&buffer[data_size], NON_DATA_SIZE);
       const voltronic_crc_t calculated_crc = calculate_voltronic_crc(buffer, data_size);
       buffer[data_size] = 0;
 
@@ -203,21 +196,21 @@ static int voltronic_write_data_loop(
   while(1) {
     const int write_result = voltronic_dev_write(dev, buffer, bytes_left);
 
-    if (write_result > 0) {
+    if (write_result >= 0) {
       bytes_left -= write_result;
       if (bytes_left > 0) {
         buffer = &buffer[write_result];
       } else {
         return buffer_length;
       }
-    } else if (write_result != 0) {
-      return write_result;
-    }
 
-    elapsed = get_millisecond_timestamp() - start_time;
-    if (elapsed >= timeout_milliseconds) {
-      SET_ERRNO(ETIMEDOUT);
-      return -1;
+      elapsed = get_millisecond_timestamp() - start_time;
+      if (elapsed >= timeout_milliseconds) {
+        SET_ERRNO(ETIMEDOUT);
+        return -1;
+      }
+    } else {
+      return write_result;
     }
   }
 }
@@ -230,11 +223,11 @@ static int voltronic_send_data(
 
   const voltronic_crc_t crc = calculate_voltronic_crc(buffer, buffer_length);
 
-  const size_t copy_length = buffer_length + MINIMUM_SIZE;
+  const size_t copy_length = buffer_length + NON_DATA_SIZE;
   char* copy = malloc(copy_length * sizeof(char));
   memcpy(copy, buffer, buffer_length * sizeof(char));
 
-  write_voltronic_crc(crc, &copy[buffer_length], MINIMUM_SIZE);
+  write_voltronic_crc(crc, &copy[buffer_length], NON_DATA_SIZE);
   copy[copy_length - 1] = END_OF_INPUT;
 
   const int result = voltronic_write_data_loop(
@@ -268,7 +261,7 @@ int voltronic_dev_execute(
 
   if (send_result > 0) {
     elapsed = get_millisecond_timestamp() - start_time;
-    if (timeout_milliseconds > elapsed) {
+    if (elapsed < timeout_milliseconds) {
       SET_ERRNO(0);
       return voltronic_receive_data(
         dev,
